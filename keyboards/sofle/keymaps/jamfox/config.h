8c1cfdbbf2925ca7ba604f5b8460cef3d2f50a35
JamFox <github@jamfox.dev>
2023-02-20 16:11:59 +0200

    sofle: add jamfox keymap

---
+++ keyboards/sofle/keymaps/jamfox/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define LEADER_TIMEOUT 250
+#define LEADER_PER_KEY_TIMING
