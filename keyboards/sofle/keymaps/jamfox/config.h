e7ad1dd9153403841db66fdd8f0de0eef5bd058f
JamFox <github@jamfox.dev>
2024-01-04 17:55:46 +0200

    update sofle keymap to newest qmk

--- keyboards/sofle/keymaps/jamfox/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
-
-#define LEADER_TIMEOUT 250
-#define LEADER_PER_KEY_TIMING
